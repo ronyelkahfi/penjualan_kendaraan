07d24cc64bf46f665a5dc159c56a5c213ff6ca73
Fatkhur Rony <ronyelkahfi@gmail.com>
2023-05-03 08:31:45 +0700

    feat : Create Kendaraan Endpoint

---
+++ app/Http/Controllers/KendaraanController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\KendaraanService;
+class KendaraanController extends Controller
+{
+    protected $kendaraanService;
+    function __construct(KendaraanService $kendaraanService){
+        $this->kendaraanService = $kendaraanService;
+    }
+    function create(Request $req){
+        $data = json_decode($req->getContent());
+        // var_dump($data);
+        return $this->kendaraanService->create($data);
+    }
+}
---
+++ app/services/KendaraanService.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Services;
+use App\Repositories\KendaraanRepository;
+use App\Services\Dto\MobilDto;
+use App\Services\Dto\MotorDto;
+class KendaraanService {
+    protected $kendaraanRepo;
+    
+    function __construct(KendaraanRepository $kendaraanRepo){
+        $this->kendaraanRepo = $kendaraanRepo;
+    }
+
+    function create($data){
+        if($data->jenis_kendaraan=="motor"){
+            $input = new MotorDto(
+                $data->tahun,
+                $data->warna,
+                $data->harga, 
+                $data->mesin,
+                $data->tipe_suspensi,
+                $data->tipe_transmisi 
+            );
+        }elseif($data->jenis_kendaraan=="mobil"){
+            $input = new MobilDto(
+                $data->tahun,
+                $data->warna,
+                $data->harga,
+                $data->mesin,
+                $data->kapasitas_penumpang,
+                $data->tipe
+            );
+        }
+        
+        $inputData = (array) $input;
+        $inputData["stok"] = $data->stok;
+        // // var_dump($inputData);
+        $result = $this->kendaraanRepo->create($inputData);
+        return $this->response(201,"Created",$result);
+    }
+
+    function response(int $code, string $message, $data){
+        return response(json_encode([
+            "status" => $code,
+            "message" => $message,
+            "data" => $data
+        ]), $code)
+        ->header('Content-Type', 'text/json');
+    }
+    function responseError(int $code){
+        return response(json_encode([
+            "status" => $code,
+            "message" => $message
+        ]), $code)
+        ->header('Content-Type', 'text/json');
+    }
+}
