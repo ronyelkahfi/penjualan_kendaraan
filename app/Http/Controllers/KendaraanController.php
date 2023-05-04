83cf1a914819672763137c12169a1406aa1c83aa
Fatkhur Rony <ronyelkahfi@gmail.com>
2023-05-05 06:46:17 +0700

    perf: implement response util for `kendaraan` endpoint response

--- app/Http/Controllers/KendaraanController.php
+++ app/Http/Controllers/KendaraanController.php
@@ -4,21 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\KendaraanService;
+use App\Utils\ResponseUtil;
 class KendaraanController extends Controller
 {
     protected $kendaraanService;
-    function __construct(KendaraanService $kendaraanService){
+    protected $responseUtil;
+    function __construct(KendaraanService $kendaraanService, ResponseUtil $response){
         $this->kendaraanService = $kendaraanService;
+        $this->responseUtil = $response;
     }
     function create(Request $req){
         $data = json_decode($req->getContent());
         // var_dump($data);
         $result = $this->kendaraanService->create($data);
-        return $this->response(201,"Created",$result);
+        return $this->responseUtil->response(201,"Created",$result);
     }
     function get(){
         $result = $this->kendaraanService->getStok();
-        return $this->response(200, "Success", $result);
+        return $this->responseUtil->response(200, "Success", $result);
     }
     function response(int $code, string $message, $data){
         return response(json_encode([
@@ -27,7 +30,7 @@ class KendaraanController extends Controller
             "data" => $data
         ]), $code)
         ->header('Content-Type', 'text/json');
-        return $this->response(201,"Created",$result);
+        return $this->responseUtil->response(201,"Created",$result);
     }
     
 }
