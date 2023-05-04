ff636b246555b3608039e153339b1998b7566c9a
Fatkhur Rony <ronyelkahfi@gmail.com>
2023-05-05 06:51:12 +0700

    perf: implement response utils for `penjualan` endpoint response

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -4,42 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\PenjualanService;
+use App\Utils\ResponseUtil;
 class PenjualanController extends Controller
 {
     protected $penjualanService;
-    
-    function __construct(PenjualanService $penjualanService){
+    protected $responseUtil;
+    function __construct(PenjualanService $penjualanService, ResponseUtil $response){
         $this->penjualanService = $penjualanService;
+        $this->responseUtil = $response;
     }
 
     function create(Request $req){
         $data = json_decode($req->getContent());
         $result = $this->penjualanService->create($data);
         if($result){
-            return $this->response(201, "Created", $result);
+            return $this->responseUtil->response(201, "Created", $result);
         }
     }
 
     function getSalesByProduct(){
         $result = $this->penjualanService->getSalesByProduct();
-        return $this->response(200,"Get data success", $result);
-    }
-    
-    function response(int $code, string $message, $data){
-        return response(json_encode([
-            "status" => $code,
-            "message" => $message,
-            "data" => $data
-        ]), $code)
-        ->header('Content-Type', 'text/json');
-        return $this->response(201,"Created",$result);
-    }
-    
-    function responseError(int $code, string $message){
-        return response(json_encode([
-            "status" => $code,
-            "message" => $message
-        ]), $code)
-        ->header('Content-Type', 'text/json');
+        return $this->responseUtil->response(200,"Get data success", $result);
     }
 }
