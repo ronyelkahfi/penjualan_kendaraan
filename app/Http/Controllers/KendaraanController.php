ca9e59f316ba9ee96f63bf397fedab92a290622d
Fatkhur Rony <ronyelkahfi@gmail.com>
2023-05-05 09:06:50 +0700

    perf: standardize create `kendaraan` response

--- app/Http/Controllers/KendaraanController.php
+++ app/Http/Controllers/KendaraanController.php
@@ -13,24 +13,24 @@ class KendaraanController extends Controller
         $this->kendaraanService = $kendaraanService;
         $this->responseUtil = $response;
     }
+    
     function create(Request $req){
         $data = json_decode($req->getContent());
-        // var_dump($data);
+        
         $result = $this->kendaraanService->create($data);
-        return $this->responseUtil->response(201,"Created",$result);
+        if($result["success"]==true){
+            return $this->responseUtil->response($result["code"],$result["message"],$result["data"]);
+        }else{
+            return $this->responseUtil->responseError($result["code"],$result["message"],$result["constraint"]);
+        }
     }
+
     function get(){
         $result = $this->kendaraanService->getStok();
-        return $this->responseUtil->response(200, "Success", $result);
+        if($result["success"]==true){
+            return $this->responseUtil->response($result["code"],$result["message"],$result["data"]);
+        }else{
+            return $this->responseUtil->responseError($result["code"],$result["message"],$result["constraint"]);
+        }
     }
-    function response(int $code, string $message, $data){
-        return response(json_encode([
-            "status" => $code,
-            "message" => $message,
-            "data" => $data
-        ]), $code)
-        ->header('Content-Type', 'text/json');
-        return $this->responseUtil->response(201,"Created",$result);
-    }
-    
 }
